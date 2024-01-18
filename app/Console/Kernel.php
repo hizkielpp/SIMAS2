5cd9f26c1b742ff7bf65d2d91d95ec80c07d7c66
musaalfian <musaganteng071@@gmail.com>
2024-01-18 15:44:15 +0700

    Complete: fixing cek hari libur pada generate antidatir (menambahkan kondisi weekend)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,102 +21,62 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
-            for ($a = 1; $a <= 10; $a++) {
-                // Ambil tanggal sekarang
-                $today = Carbon::now();
-                // Sesuaikan format
-                $todayFormated = $today->format('Y-m-d');
-                $output = new ConsoleOutput();
-                $max = DB::table('suratkeluar')
-                    ->where('tanggalPengesahan', $todayFormated)
-                    ->max('nomorSurat');
+            // Ambil tanggal sekarang
+            $today = Carbon::now();
+            // Sesuaikan format
+            $todayFormated = $today->format('Y-m-d');
+            $output = new ConsoleOutput();
+            $max = DB::table('suratkeluar')
+                ->where('tanggalPengesahan', $todayFormated)
+                ->max('nomorSurat');
 
-                // Cek untuk menghindari hari sabtu/minggu dan hari libur nasional
-                $client = new Client();
-                $response = $client->get('https://api-harilibur.vercel.app/api', [
-                    'headers' => [
-                        'accept' => 'application/json'
-                    ]
-                ]);
-                $data = json_decode($response->getBody(), true);
-                $false = true;
-                foreach ($data as $value) {
-                    if (date('Y-m-d') == $value['holiday_date']) {
-                        $false = false;
-                    };
-                }
-                if ($false) {
-                    for ($a = 1; $a <= 10; $a++) {
-                        try {
-                            DB::table('suratkeluar')->insert([
-                                'tanggalPengesahan' => $todayFormated,
-                                'created_at' => $todayFormated,
-                                'updated_at' => $todayFormated,
-                                'nomorSurat' => $max + $a,
-                                'kodeHal' => 'AK',
-                                'sifatSurat' => 1,
-                                'jenis' => 'antidatir',
-                                'status' => 'belum',
-                                'tujuanSurat' => 'dummy',
-                                'lampiran' => 'dummy',
-                                'jumlahLampiran' => 1,
-                                'created_by' => 777,
-                                'kodeUnit' => 'dummy',
-                                'disahkanOleh' => 'dummy'
-                            ]);
-                        } catch (\Exception $e) {
-                            $output->writeln("<info>" . $e  . "</info>");
-                        }
+            // Ambil data hari libur menggunakan API
+            $client = new Client();
+            $response = $client->get('https://api-harilibur.vercel.app/api', [
+                'headers' => [
+                    'accept' => 'application/json'
+                ]
+            ]);
+            $data = json_decode($response->getBody(), true);
+
+            // Cek untuk menghindari hari sabtu/minggu dan hari libur nasional
+            $isWorkingDay = true;
+            foreach ($data as $value) {
+                // Ambil tanggal hari libur dari API
+                $dateOriginal = Carbon::createFromFormat('Y-m-d', $value['holiday_date']);
+                // Sesuaikan format dengan format tanggal di API
+                $value['holiday_date'] = $dateOriginal->format('Y-m-d');
+                if ((($todayFormated == $value['holiday_date']) && $value['is_national_holiday']) || ($today == $today->isWeekend())) {
+                    $isWorkingDay = false;
+                };
+            }
+
+            // Jika hari kerja, maka generate antidatir
+            if ($isWorkingDay) {
+                for ($i = 1; $i <= 10; $i++) {
+                    try {
+                        DB::table('suratkeluar')->insert([
+                            'tanggalPengesahan' => $todayFormated,
+                            'created_at' => $todayFormated,
+                            'updated_at' => $todayFormated,
+                            'nomorSurat' => $max + $i,
+                            'kodeHal' => 'AK',
+                            'sifatSurat' => 1,
+                            'jenis' => 'antidatir',
+                            'status' => 'belum',
+                            'tujuanSurat' => 'dummy',
+                            'lampiran' => 'dummy',
+                            'jumlahLampiran' => 1,
+                            'created_by' => 777,
+                            'kodeUnit' => 'dummy',
+                            'disahkanOleh' => 'dummy'
+                        ]);
+                    } catch (\Exception $e) {
+                        $output->writeln("<info>" . $e  . "</info>");
                     }
-                } else {
                 }
             }
-            // // Ambil tanggal sekarang
-            // $today = Carbon::now();
-            // // Sesuaikan format
-            // $todayFormated = $today->format('Y-m-d');
-
-            // // Ambil max nomor surat antidatir
-            // $max = DB::table('suratkeluar')
-            //     ->where('tanggalPengesahan', $todayFormated)
-            //     ->max('nomorSurat');
-
-            // // Cek untuk menghindari hari sabtu/minggu dan hari libur nasional
-            // $client = new Client();
-            // $response = $client->get('https://api-harilibur.vercel.app/api', [
-            //     'headers' => [
-            //         'accept' => 'application/json'
-            //     ]
-            // ]);
-            // $data = json_decode($response->getBody(), true);
-            // $false = true;
-            // foreach ($data as $value) {
-            //     if (date('Y-m-d') == $value['holiday_date']) {
-            //         $false = false;
-            //     };
-            // }
-            // if ($false) {
-            //     for ($a = 1; $a <= 10; $a++) {
-            //         DB::table('suratkeluar')->insert([
-            //             'tanggalPengesahan' => $todayFormated,
-            //             'created_at' => $todayFormated,
-            //             'updated_at' => $todayFormated,
-            //             'nomorSurat' => $max + $a,
-            //             'kodeHal' => 'dummy',
-            //             'sifatSurat' => 'biasa',
-            //             'jenis' => 'antidatir',
-            //             'status' => 'belum',
-            //             'tujuanSurat' => 'dummy',
-            //             'lampiran' => 'dummy',
-            //             'jumlahLampiran' => 1,
-            //             'created_by' => 1,
-            //             'kodeUnit' => 'dummy',
-            //             'disahkanOleh' => 'dummy'
-            //         ]);
-            //     }
-            // } else {
-            // }
-        })->everyMinute()->weekdays()->timezone('Asia/Jakarta');
+        })->dailyAt('23:59')->timezone('Asia/Jakarta');
     }
 
     /**
